1abcad2123d790f33abaa4e3a6e48e8593fa73f2
developer.li1988 <developer.li1988@gmail.com>
2015-07-29 15:16:23 +0800

    fourth edit calendars controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,6 +4,27 @@ class CalendarsController < ApplicationController
   def index 
     yyyymm = params[:yyyymm]
     @calendars = current_ou.calendars.search(yyyymm)
+
+    if @calendars.size == 0 && yyyymm != nil
+      year = yyyymm[0..3]
+      month = yyyymm[4..5]
+      check_date = Date.parse("#{year}/#{month}") rescue nil
+      if check_date
+        days = Time.days_in_month(month.to_i, year.to_i)
+        days.times do |day|
+          full_date = "#{year}/#{month}/#{day+1}"
+          week = Date.parse(full_date).strftime("%A")
+          if week == "Sunday" || week == "Saturday"
+            is_holiday = 1
+          else
+            is_holiday = 0
+          end
+          Calendar.create(ou_id: current_ou.id, duty_date: full_date, is_holiday: is_holiday)
+        end
+      else
+        redirect_to calendars_path, notice: '日期格式不正確'
+      end
+    end
   end
 
   def edit
