d135cbc06f0a821d82163cf269b00b8cca63e59c
genjay <genjay@gmail.com>
2015-09-22 15:45:44 +0800

    fix timezone bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # ActiveRecord::Base.default_timezone = :utc
-  config.time_zone= 'Taipei'
+  # config.time_zone= 'Taipei' #會讓時間錯亂不要開
   
 end
